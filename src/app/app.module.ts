1695d76de1e899b10e41e3c688922cfd34b45501
Karly Lamm <klamm@voltsystems.net>
2021-04-06 10:47:48 -0500

    moved header into its own component and declared components for other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, FooterComponent, CheckoutComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
